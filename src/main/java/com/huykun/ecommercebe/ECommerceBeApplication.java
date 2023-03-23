ebccd4e682c5f249ef759f63f9a62ea51da6b848
DQHUY <dinh.quang.huy@quantic.com.vn>
2023-03-23 14:12:35 +0700

    asdas

---
+++ src/main/java/com/huykun/ecommercebe/ECommerceBeApplication.java
@@ -0,0 +1,13 @@
+package com.huykun.ecommercebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceBeApplication.class, args);
+	}
+
+}
